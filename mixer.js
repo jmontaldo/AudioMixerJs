1dedbddef4b569c3a51f86e87df6caab75e0a2e4
Juan Montaldo <juanjosemontaldo@hotmail.com>
2024-08-07 15:16:01 -0300

    Se agrega el Channel 2

--- mixer.js
+++ mixer.js
@@ -1,40 +1,30 @@
+var channel_1 = [];
+var channel_2 = [];
 var control = 0;
-var myAudioContext, gain_node, pan_node, low_eq_node, file_1, track_1, play_button, maximo, progreso, bucle, tiempo_actual, tiempo_total, pan_ch1, volumen_ch1;
-var low_eq_node, eq_ch1_lf, hf_eq_node, eq_ch1_hf, mb_eq_node, eq_ch1_mf, mute_button_ch1;
-
+var maximo = 120;
+var maximo, progreso, bucle, tiempo_actual, tiempo_total, play_button, myAudioContext;
+var file_1, track_1, eq_ch1_lf, eq_ch1_hf, eq_ch1_mf, mute_button_ch1, pan_ch1, volumen_ch1;
+var file_2, track_2, pan_ch2;
 
 function iniciar(){
-    maximo = 120;
-    myAudioContext = new AudioContext();
-    gain_node = myAudioContext.createGain();
-    pan_node = myAudioContext.createStereoPanner();
-    low_eq_node = myAudioContext.createBiquadFilter();
-    hf_eq_node = myAudioContext.createBiquadFilter();
-    mb_eq_node = myAudioContext.createBiquadFilter();
-
-    file_1 = document.getElementById("file-ch1");
-    track_1 = myAudioContext.createMediaElementSource(file_1);
-    track_1.connect(gain_node).connect(pan_node).connect(low_eq_node).connect(mb_eq_node).connect(hf_eq_node).connect(myAudioContext.destination); 
-
-    low_eq_node.type = "lowshelf";
-    low_eq_node.frequency.value = 100;
-    mb_eq_node.type = "peaking";
-    mb_eq_node.Q.value = 1;
-    mb_eq_node.frequency.value = 2500;
-    hf_eq_node.type = "highshelf";
-    hf_eq_node.frequency.value = 10000;
-    
+    // REPRODUCTOR
     progreso = document.getElementById("progress-bar");
     tiempo_actual = document.getElementById("current-time");
     tiempo_total = document.getElementById("duration");
     play_button = document.getElementById("play-button");
+    play_button.addEventListener("click", reproducir);
+    // CHANNEL 1
+    myAudioContext = new AudioContext();
+    channel_1 = channel(myAudioContext);
+    file_1 = document.getElementById("file-ch1");
+    track_1 = myAudioContext.createMediaElementSource(file_1);
+    track_1.connect(channel_1[0]).connect(channel_1[1]).connect(channel_1[2]).connect(channel_1[3]).connect(channel_1[4]).connect(myAudioContext.destination); 
     mute_button_ch1 = document.getElementById("mute-button-ch1");
     pan_ch1 = document.getElementById("pan-ch1");
     eq_ch1_lf = document.getElementById("eq-ch1-lf");
     eq_ch1_mf = document.getElementById("eq-ch1-mf");
     eq_ch1_hf = document.getElementById("eq-ch1-hf");
     volumen_ch1 = document.getElementById("fader-ch1");
-    play_button.addEventListener("click", reproducir);
     mute_button_ch1.addEventListener("click", function(evento){
         control += 1;
         mutear(evento, control);
@@ -44,6 +34,31 @@ function iniciar(){
     eq_ch1_mf.addEventListener("change", mf_eq);
     eq_ch1_hf.addEventListener("change", hi_eq);
     volumen_ch1.addEventListener("change", volumen);
+    // CHANNEL 2
+    channel_2 = channel(myAudioContext);
+    file_2 = document.getElementById("file-ch2");
+    track_2 = myAudioContext.createMediaElementSource(file_2);
+    track_2.connect(channel_2[0]).connect(channel_2[1]).connect(channel_2[2]).connect(channel_2[3]).connect(channel_2[4]).connect(myAudioContext.destination);
+    pan_ch2 = document.getElementById("pan-ch2");
+    pan_ch2.addEventListener("change", panear);
+}
+
+function channel(myAudioContext){
+    var channel_array = [];
+    var gain_node = myAudioContext.createGain();
+    var pan_node = myAudioContext.createStereoPanner();
+    var low_eq_node = myAudioContext.createBiquadFilter();
+    var hf_eq_node = myAudioContext.createBiquadFilter();
+    var mb_eq_node = myAudioContext.createBiquadFilter();
+    low_eq_node.type = "lowshelf";
+    low_eq_node.frequency.value = 100;
+    mb_eq_node.type = "peaking";
+    mb_eq_node.Q.value = 1;
+    mb_eq_node.frequency.value = 2500;
+    hf_eq_node.type = "highshelf";
+    hf_eq_node.frequency.value = 10000;
+    channel_array = [gain_node, pan_node, low_eq_node, mb_eq_node, hf_eq_node];
+    return channel_array;
 }
 
 function reproducir(){
@@ -53,10 +68,12 @@ function reproducir(){
     if(!file_1.paused && !file_1.ended){
         play_button.innerHTML = ">";
         file_1.pause();
+        file_2.pause();
         clearInterval(bucle);
     }else{
         play_button.innerHTML = "||";
         file_1.play();
+        file_2.play();
         bucle = setInterval(estado, 1000);
     }
 }
@@ -74,31 +91,35 @@ function estado(){
     }
 }
 
-function panear(){
-    pan_node.pan.value = pan_ch1.value;
+function volumen(){
+    channel_1[0].gain.value = volumen_ch1.value;
+}
+
+function panear(evento){
+    if(evento.target.id == "pan-ch1"){
+        channel_1[1].pan.value = pan_ch1.value;
+    } else {
+        channel_2[1].pan.value = pan_ch2.value;
+    }
 }
 
 function low_eq(){
-    low_eq_node.gain.value = eq_ch1_lf.value;
+    channel_1[2].gain.value = eq_ch1_lf.value;
 }
 
 function mf_eq(){
-    mb_eq_node.gain.value = eq_ch1_mf.value;
+    channel_1[3].gain.value = eq_ch1_mf.value;
 }
 
 function hi_eq(){
-    hf_eq_node.gain.value = eq_ch1_hf.value;
-}
-
-function volumen(){
-    gain_node.gain.value = volumen_ch1.value;
+    channel_1[4].gain.value = eq_ch1_hf.value;
 }
 
 function mutear(evento, control){
     if((control % 2) == 0){
-        gain_node.gain.value = volumen_ch1.value;
+        channel_1[0].gain.value = volumen_ch1.value;
     } else {
-        gain_node.gain.value = 0;
+        channel_1[0].gain.value = 0;
     }
 }
 
